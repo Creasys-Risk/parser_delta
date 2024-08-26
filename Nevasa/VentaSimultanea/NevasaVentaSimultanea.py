d5419e71ebd5961ee0f41fca3459e1d641ce3593
DVillegasA <dvillegas@creasys.cl>
2024-08-26 17:03:15 -0400

    20240826: Resultados Venta de Simultaneas Nevasa
    Además se corrige el parser para acomodar a los nuevos casos e incluir un control de excepciones menos estricto

--- Nevasa/VentaSimultanea/NevasaVentaSimultanea.py
+++ Nevasa/VentaSimultanea/NevasaVentaSimultanea.py
@@ -12,40 +12,47 @@ def nevasa_venta_simultanea_parser(process_date: str, main_folder: str):
         result = []
 
         for file in files: 
-            reader = PdfReader(folder+file)
+            try:
+                reader = PdfReader(folder+file)
 
-            text = ''
+                text = ''
 
-            for page_num in range(len(reader.pages)):
-                page = reader.pages[page_num]
-                text += page.extract_text()
+                for page_num in range(len(reader.pages)):
+                    page = reader.pages[page_num]
+                    text += page.extract_text()
 
-            _, data = text.split('Fecha Pago: ')
-            fecha, data = data.split('\n', maxsplit=1)
-            fecha = datetime.strptime(fecha, "%d/%m/%Y").date()
-            _, data = data.split('COMPRA VENTA\n')
-            data, _ = data.split('SUB TOTAL(1)')
-            data_rows = data.split('\n')[:-1]
+                _, data = text.split('Fecha Pago: ')
+                fecha, data = data.split('\n', maxsplit=1)
+                fecha = datetime.strptime(fecha, "%d/%m/%Y").date()
+                _, data = data.split('COMPRA VENTA\n')
+                data, _ = data.split('SUB TOTAL(1)')
+                data_rows = data.split('\n')[:-1]
 
-            for row in data_rows:
-                row = row.replace('VENTA ', '')
-                row = row.replace('AC ', '')
-                row = row.replace('OD ', '')
-                row = row.split(' ')
-                monto = int(row[len(row)-1].replace('.', ''))
-                cantidad = int(row[len(row)-2].replace('.', ''))
-                row = row[:-6]
-                row = row[1:]
-                precio = float(row.pop(len(row)-1).replace('%', '').replace(',', '.'))
-                nemotecnico = ' '.join(row)
-                
-                result.append({
-                    "fecha": fecha,
-                    "nemotecnico": nemotecnico,
-                    "precio": precio,
-                    "cantidad": cantidad,
-                    "monto": monto,
-                })
+                for row in data_rows:
+                    row = row.replace('VENTA ', '')
+                    row = row.replace('AC ', '')
+                    row = row.replace('OD ', '')
+                    row = row.replace('CF ', '')
+                    row = row.replace('COR ', '')
+                    row = row.replace('Cust ', '')
+                    row = row.split(' ')
+                    monto = int(row[len(row)-1].replace('.', ''))
+                    cantidad = int(row[len(row)-2].replace('.', ''))
+                    row = row[:-5]
+                    row = row[1:]
+                    precio = float(row.pop(len(row)-1).replace('%', '').replace(',', '.'))
+                    nemotecnico = ' '.join(row)
+                    
+                    result.append({
+                        "fecha": fecha,
+                        "nemotecnico": nemotecnico,
+                        "precio": precio,
+                        "cantidad": cantidad,
+                        "monto": monto,
+                    })
+            except Exception as err:
+                print(f"Error en Parser de Venta de Simultaneas Nevasa, archivo {file}: {err}")
+                continue
 
         df = pd.DataFrame(result)
 
